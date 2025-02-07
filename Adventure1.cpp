2963d2d3424f070e52eee7d617f63c28782e6b1e
0803bella <0803bella@gmail.com>
2025-02-07 15:02:15 -0600

    Completed adventure1

--- Adventure1.cpp
+++ Adventure1.cpp
@@ -18,44 +18,43 @@ int main() {
     cout << "Player's health = " << playerHealth << ".\n";
 
 //seed the random number generator
-    string input = "";
-    int randomNumber = (rand() % 5);
-
 //start the loop
     bool playerIsAlive = true;
     int turns = 0;
 
 //add 1 to turns
-    while (playerIsAlive) {
+    while (playerHealth > 0 && turns < 4) {
         string input;
         turns += 1;
         if (debug) cout << "Turn: " << turns << "\n";
         cin >> input;
-   }
 
 //start the encounter
 //randomly generate numbers for attack (range = 0-4) and block (range = 0-4)
-
-    cout << "Would you like to attack the enemy or block its attack?\n";
-    cin >> input;
-
-    if (input == "attack"){
-        cout << "You attacked the enemy!\n";
-        cout << "Attack landed " << randomNumber << " hit points!\n";
-        cin >> input;
-    }
-    if (input == "block"){
-        cout << "You blocked the enemy's attack!\n";
-        cout << "You blocked " << randomNumber << " hit points!\n";
-        cin >> input;
-    }
-    else {
-        cout << "That is not an option!\n";
-    }
 //        if block is greater or equal to attack, successful block
 //        otherwise, subtract attack value from health.
+
+    int attack = rand() % 5;
+    int block = rand() % 5;
+
+    if (block >= attack) {
+        cout << "Succesful block!\n";
+    }
+    else {
+        playerHealth -= attack;
+        cout << "You were hit!\n";
+        cout << "Your health is now: " << playerHealth << ".\n";
+    }
 //        ask the player if they would like to keep adventuring. if they don't, break out of the loop.
 //keep looping while health is greater than zero and fewer than 4 turns have happened
+
+    }
 //if health is greater than 0, congratulate player
+    if (playerHealth > 0) {
+        cout << "Congrats! You survived!\n";
+    }
 //otherwise, tell the player they're dead.
+    if (playerHealth < 0) {
+        cout << "You died!\n";
+    }
 }
